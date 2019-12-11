bb0f1d19cba975f1b1044689f720f58097687994
adrianmaganyaguerrero <adrimaga2000@gmail.com>
2019-12-11 11:02:37 +0100

    Segunda activity + Imagenes

--- app/src/main/java/cat/copernic/erick/projectm07/MainActivity.java
+++ app/src/main/java/cat/copernic/erick/projectm07/MainActivity.java
@@ -2,7 +2,9 @@ package cat.copernic.erick.projectm07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void abrirRegistro(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
 }
-//haawdhaffaasfasf
