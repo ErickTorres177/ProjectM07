f03c414a8acfb9c11ca3f489251061681700aa5a
ErickTorres177 <ericktorres.17ea@gmail.com>
2020-02-13 13:09:43 +0100

    Arreglos

--- app/src/main/java/cat/copernic/erick/projectm07/ui/Rutas/RutasFragment.java
+++ app/src/main/java/cat/copernic/erick/projectm07/ui/Rutas/RutasFragment.java
@@ -77,7 +77,7 @@ public class RutasFragment extends Fragment {
         btnCancela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
